15ae245b8b6e0a0446972a9febdd906c8ade2674
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-17 21:02:20 +0200

    display the contents of products dynamically in index.blade.php

--- oStudio/resources/views/user/index.blade.php
+++ oStudio/resources/views/user/index.blade.php
@@ -1,5 +1,17 @@
 @extends('user.layout.base')
 
 @section('content')
+    @if (count($products) > 0)
+        <ul>
+            @foreach ($products as $product)
+                <div class="jumbotron">
+                        <li>{{$product->name}}</li>
+                <small><p>{{$product->description}}</p></small>
+                </div>    
+            @endforeach
+        </ul>    
+    @else
+        <p>No Products Found!</p>
+    @endif
     
 @endsection

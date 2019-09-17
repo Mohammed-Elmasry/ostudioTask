e53146de82b33e4c565787aa140baadce96bd991
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-17 19:49:32 +0200

    create base.blade.php for users layouts

---
+++ oStudio/resources/views/user/layout/base.blade.php
@@ -0,0 +1,44 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>{{config('app.name')}}</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
+    <link rel='stylesheet' type='text/css' href="{{asset('css/app.css')}}">
+    <script src='main.js'></script>
+</head>
+<body>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="/">{{config('app.name')}}</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class="navbar-toggler-icon"></span>
+                </button>
+              
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav mr-auto">
+                    <li class="nav-item">
+                      <a class="nav-link" href="/">Home <span class="sr-only"></span></a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="about">About</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="services">Services</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="posts">Posts</a>
+                    </li>
+                    <li class="nav-item">
+                      <a class="nav-link" href="contact">Contact Us</a>
+                    </li>
+                  </ul>
+                </div>
+              </nav>
+    <div class="container">
+      @yield('content')
+    </div>
+</body>
+</html>

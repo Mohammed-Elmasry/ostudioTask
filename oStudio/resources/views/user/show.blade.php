7355271c0507da9e31e96970e101d429b6c980b9
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-17 21:27:06 +0200

    define html in the show.blade.php view

--- oStudio/resources/views/user/show.blade.php
+++ oStudio/resources/views/user/show.blade.php
@@ -1,5 +1,9 @@
 @extends('user.layout.base')
 
 @section('content')
-    
+<h1>{{$product->name}}</h1>
+<div>
+    <small>{{$product->description}}</small><br/>
+    <img alt="image of product {{$product->id}}"src="{{$product->image}}">
+</div>
 @endsection

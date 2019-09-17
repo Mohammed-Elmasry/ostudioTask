5993a2119b6eac82512c571ac82e90b63f508605
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-17 19:40:12 +0200

    divide format of routes in web.php

--- oStudio/routes/web.php
+++ oStudio/routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/admin/dashboard', 'AdminsController@dashboard');
-
+//user routes
 Route::get('/products', 'ProductsController@index')->name("products");
+
+//admin routes
+Route::get('/admin/dashboard', 'AdminsController@dashboard');

aa7d4d7731c0affa354e91dafe761a38684ec432
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-18 18:09:46 +0200

    create a product instance within the products component

--- front-oStudio/src/app/products/products.component.ts
+++ front-oStudio/src/app/products/products.component.ts
@@ -1,12 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import {Product} from '../Product';
 
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
   styleUrls: ['./products.component.scss']
 })
+
 export class ProductsComponent implements OnInit {
-  hero = "Windstorm";
+  product: Product = {
+    id: 1,
+    name: 'ORM',
+    description: 'Object relational mapper',
+    image: ''
+  };
+
+
   constructor() { }
 
   ngOnInit() {

f312f49f6d2181d1df2870b987ecb35ed14840ab
Mohammed-Elmasry <mohammed.hussein.business@gmail.com>
2019-09-18 14:04:11 +0200

    create the edit.blade.php template

--- oStudio/resources/views/admin/edit.blade.php
+++ oStudio/resources/views/admin/edit.blade.php
@@ -1,5 +1,23 @@
 @extends('admin.layout.app')
 
 @section('content')
-    
+<h1>Update Product</h1>
+<form method="post" action="/admin/{{$product->id}}/edit" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+          <label for="name">Product name</label>
+          <input name="name" type="text" class="form-control" aria-describedby="emailHelp" placeholder="product name.." required>
+          <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+        </div>
+        <div class="form-group">
+          <label for="description">Description</label>
+          <textarea name="description" class="form-control" id="exampleInputPassword1" rows="10" placeholder="Description" required></textarea>
+        </div>
+        <div class="form-group">
+            <label for="img">Image</label>
+            <input type="file" name="img"/>
+        </div>
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+
 @endsection
